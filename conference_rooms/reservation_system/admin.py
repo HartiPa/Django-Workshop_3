c466438bc633dce4c9a920466b8f1ac94fceffff
Matej Myslik <myslikmatej@gmail.com>
2024-11-16 11:42:43 +0100

    Django-Workshop_3
    1.- Setting the display of tables and values ​​in /admin/

--- conference_rooms/reservation_system/admin.py
+++ conference_rooms/reservation_system/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Equipment)
-admin.site.register(Rooms)  # Registrace s vlastní konfigurací
 
+class RoomsAdmin(admin.ModelAdmin):
+    """
+    Display in table and values of Rooms in /admin/
+    """
+    list_display = ("name", "capacity", "get_equipments")
+    search_fields = ("name",)
+    list_filter = ("equipments",)
+
+    def get_equipments(self, obj):
+        return ", ".join([equipment.name for equipment in obj.equipments.all()])
+    get_equipments.short_description = "Equipments"
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    """
+    Display in table and values of Equipments in /admin/
+    """
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+admin.site.register(Rooms, RoomsAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
--- conference_rooms/reservation_system/views.py
+++ conference_rooms/reservation_system/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+
